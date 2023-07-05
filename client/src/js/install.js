90f2f3517a1c2728406ee92813c80e06c3fa2c00
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-05 21:47:18 +1000

    wip

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,6 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log("beforeinsallprompt");
     // Store the triggered events
     window.deferredPrompt = event;
 
@@ -12,6 +13,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    console.log("click");
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
@@ -29,6 +31,7 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    console.log("afterinstalled");
     // Clear prompt
     window.deferredPrompt = null;
 });

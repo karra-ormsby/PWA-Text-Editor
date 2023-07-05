6330a203d01b008401b2cde84d4b978b140d029a
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-05 13:37:34 +1000

    trying to add service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -20,10 +19,9 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content }); 
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('Data saved to the database');
 }
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
